cc09b9241d07acfa26e54f43802b85392a74de37
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-09-21 22:18:20 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,7 +5,7 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
 
-  def display_@board()
+  def display_board()
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
